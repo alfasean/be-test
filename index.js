237706ac6e6ce10406ba1ce069148ca79c70784d
Your Name <alfasean22@gmail.com>
2024-06-12 23:50:09 +0800

    update

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const db = mysql.createConnection({
   host: process.env.MYSQL_ADDON_HOST,  
   user: process.env.MYSQL_ADDON_USER,  
   password: process.env.MYSQL_ADDON_PASSWORD,  
-  database: process.env.MYSQL_ADDON_DB  
+  database: process.env.MYSQL_ADDON_DB,
+  database: process.env.MYSQL_ADDON_URI
 });
 
 db.connect(err => {
